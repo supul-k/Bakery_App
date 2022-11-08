0dd94afb4d571ef02f74739725cf0920f42c5cff
Supul Kalhara <supulkalhara7@gmail.com>
2022-11-08 15:03:32 +0530

    activity_main page

---
+++ app/src/main/java/com/example/tha_app_194125d/add_cake.java
@@ -0,0 +1,14 @@
+package com.example.tha_app_194125d;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_cake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cake);
+    }
+}
