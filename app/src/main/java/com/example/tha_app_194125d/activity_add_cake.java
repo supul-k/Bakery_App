1e601581ddad59c32ac54825477a1d7699f57c65
Supul Kalhara <supulkalhara7@gmail.com>
2022-11-09 13:19:30 +0530

    DBHelper

--- app/src/main/java/com/example/tha_app_194125d/activity_add_cake.java
+++ app/src/main/java/com/example/tha_app_194125d/activity_add_cake.java
@@ -3,14 +3,17 @@ package com.example.tha_app_194125d;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_add_cake extends AppCompatActivity {
 
+    EditText name, desc , price;
+    Button insert, update, delete , view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_cake);
     }
 }
